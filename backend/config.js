fbc64a6e9943c18689e2dc701e45af4ddb59b92d
neozenweb <37545009+neozenweb@users.noreply.github.com>
2019-02-03 15:00:35 -0500

    With gitignore

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    
-    ApplicationName: "EduPath",
-APIClientID: "7Y8BWoepP91LBNeNp9vEpVSmMja4bESJzf9fYyF2",
-APIClientSecret: "Q6rWQTvQs8XY2VWanJR3vbKSzMqcNW2mDqsf9A2R8RzFX0lhLSL7kOrpYxrveUACMUMPUr6O76nV2EuyN8deSPyHDm8BH5zwa66rPTUrVxXLPzoKQ3la4Z9ezDRMrHuI"
-
-}
